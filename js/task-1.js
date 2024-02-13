b46704ce16cde950701ad05cc05b0ab16edb949a
Christina Mikheyeva <c.mikheyeva@gmail.com>
2024-02-13 12:19:00 +0100

    More options added to task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,5 @@
+//----------------------- First option (my favorite)--------------------------------------
+
 function isEnoughCapacity(products, containerSize) {
     let sumProducts = 0;
     for (const product in products) {
@@ -5,7 +7,6 @@ function isEnoughCapacity(products, containerSize) {
     }
     return sumProducts > containerSize ? "false" : "true";
 }
-
 
 console.log(
   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
@@ -22,3 +23,59 @@ console.log(
 console.log(
   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
 ); // false
+
+//----------------------- Second best --------------------------------------
+
+// function isEnoughCapacity(products, containerSize) {
+//     const keys = Object.values(products);
+//     let total = 0;
+//     for (const number of keys) {
+//         total += number
+//     }
+//  return total > containerSize ? "false" : "true";
+// }
+
+// console.log(
+//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+// ); // true
+
+// console.log(
+//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+// ); // false
+
+// console.log(
+//   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+// ); // true
+
+// console.log(
+//   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+// ); // false
+
+//----------------------- Third option --------------------------------------
+
+// function isEnoughCapacity(products, containerSize) {
+//     const keys = Object.keys(products);
+//     let total = 0;
+//     for (const number of keys) {
+//         total += products[number]
+//     }
+//  return total > containerSize ? "false" : "true";
+// }
+
+
+// console.log(
+//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+// ); // true
+
+// console.log(
+//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+// ); // false
+
+// console.log(
+//   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+// ); // true
+
+// console.log(
+//   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+// ); // false
+
